33ad3168b8327da2372631b2c58c813f3385981b
Hyojeong Lee <hyojeong5663@gmail.com>
2018-06-18 16:01:16 +0900

    hj: complete hw3 at hj_submit

---
+++ DS_hw3_hj_submit/linkedlists-v2/lazy-list/linkedlist-lock.h
@@ -0,0 +1,116 @@
+/*
+ * File:
+ *   linkedlist-lock.c
+ * Author(s):
+ *   Vincent Gramoli <vincent.gramoli@epfl.ch>
+ * Description:
+ *   Lock-based linked list implementation of an integer set
+ *
+ * Copyright (c) 2009-2010.
+ *
+ * linkedlist-lock.c is part of Synchrobench
+ * 
+ * Synchrobench is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <getopt.h>
+#include <limits.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdint.h>
+/* for HTM */
+#include <immintrin.h>
+
+#include <atomic_ops.h>
+
+#define DEFAULT_DURATION                10000
+#define DEFAULT_INITIAL                 256
+#define DEFAULT_NB_THREADS              4
+#define DEFAULT_RANGE                   0x7FFFFFFF
+#define DEFAULT_SEED                    0
+#define DEFAULT_UPDATE                  20
+#define DEFAULT_LOCKTYPE	    	2
+#define DEFAULT_ALTERNATE	        0
+#define DEFAULT_EFFECTIVE	 	1
+
+#define XSTR(s)                         STR(s)
+#define STR(s)                          #s
+
+#define ATOMIC_CAS_MB_FBAR(a, e, v)     (AO_compare_and_swap_full((volatile AO_t *)(a), (AO_t)(e), (AO_t)(v)))
+
+#define ATOMIC_CAS_MB_NOBAR(a, e, v)    (AO_compare_and_swap((volatile AO_t *)(a), (AO_t)(e), (AO_t)(v)))
+
+static volatile AO_t stop;
+
+#define TRANSACTIONAL                   d->unit_tx
+
+#define TRUE							1
+#define FALSE							0
+
+typedef intptr_t val_t;
+#define VAL_MIN                         INT_MIN
+#define VAL_MAX                         INT_MAX
+
+#ifdef MUTEX
+typedef int ptlock_t;
+#define INIT_LOCK(lock)	{				\
+	*(volatile int *)lock = 0;			\
+}										
+
+
+#define DESTROY_LOCK(lock)				/*do nothing*/
+#define LOCK(lock)						/*do nothing*/
+#define UNLOCK(lock)					/*do nothing*/
+#define HLE_LOCK(lock, count) {					\
+	while(__atomic_exchange_n((volatile int *)lock, 1, __ATOMIC_ACQUIRE | __ATOMIC_HLE_ACQUIRE)){	\
+	   do {																							\
+			*((int *)count)+= 1;																		\
+			_mm_pause();																			\
+	   }while(*(volatile int *)lock == 1);															\
+	}																								\
+}										
+
+#define HLE_UNLOCK(lock) {					\
+	__atomic_store_n((volatile int *)lock, 0, __ATOMIC_RELEASE | __ATOMIC_HLE_RELEASE);				\
+}										
+
+#else
+typedef pthread_spinlock_t ptlock_t;
+#  define INIT_LOCK(lock)				pthread_spin_init((pthread_spinlock_t *) lock, PTHREAD_PROCESS_PRIVATE);
+#  define DESTROY_LOCK(lock)			pthread_spin_destroy((pthread_spinlock_t *) lock)
+#  define LOCK(lock)					pthread_spin_lock((pthread_spinlock_t *) lock)
+#  define UNLOCK(lock)					pthread_spin_unlock((pthread_spinlock_t *) lock)
+#endif
+
+typedef struct node_l {
+  val_t val;
+  struct node_l *next;
+  volatile ptlock_t lock;
+} node_l_t;
+
+typedef struct intset_l {
+  node_l_t *head;
+} intset_l_t;
+
+
+
+node_l_t *new_node_l(val_t val, node_l_t *next, int transactional);
+intset_l_t *set_new_l();
+void set_delete_l(intset_l_t *set);
+int set_size_l(intset_l_t *set);
+void node_delete_l(node_l_t *node);
+
+
---
+++ DS_hw3_hj_submit/linkedlists-v2/lockfree-list/intset.c
@@ -0,0 +1,266 @@
+/*
+ *  intset.c
+ *  
+ *  Integer set operations (contain, insert, delete) 
+ *  that call stm-based / lock-free counterparts.
+ *
+ *  Created by Vincent Gramoli on 1/12/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "intset.h"
+
+int set_contains(intset_t *set, val_t val, int transactional)
+{
+	int result;
+
+#ifdef DEBUG
+	//	printf("++> set_contains(%d)\n", (int)val);
+	//	IO_FLUSH;
+#endif
+
+#ifdef SEQUENTIAL
+	node_t *prev, *next;
+
+	prev = set->head;
+	next = prev->next;
+	while (next->val < val) {
+		prev = next;
+		next = prev->next;
+	}
+	result = (next->val == val);
+
+#elif defined STM			
+	unsigned status;
+	node_t *prev, *next;
+	val_t v = 0;
+
+	if (transactional > 1) {
+//added by charlie
+		if((status = _xbegin()) == _XBEGIN_STARTED){
+			prev = set->head;
+			next = (node_t *)TX_LOAD(&prev->next);
+			while (1) {
+				v = TX_LOAD((uintptr_t *) &next->val);
+				if (v >= val)
+					break;
+				prev = next;
+				next = (node_t *)TX_LOAD(&prev->next);
+			}		  
+			result = (v == val);
+			_xend();
+			//printf("contains commited\n");
+		}else{
+		//	printf("contains abort\n");
+			result = harris_find(set, val);
+		}
+//charlie finish
+	} else {
+//		printf("set_contains no transactional\n");
+		TX_START(NL);
+		prev = set->head;
+		next = (node_t *)TX_LOAD(&prev->next);
+		while (1) {
+			v = TX_LOAD((uintptr_t *) &next->val);
+			if (v >= val)
+				break;
+			prev = next;
+			next = (node_t *)TX_LOAD(&prev->next);
+		}		  
+		TX_END;
+		result = (v == val);
+	}
+
+
+#elif defined LOCKFREE			
+	//	result = harris_find(set, val);
+#endif	
+
+	return result;
+}
+
+inline int set_seq_add(intset_t *set, val_t val)
+{
+	int result;
+	node_t *prev, *next;
+
+	prev = set->head;
+	next = prev->next;
+	while (next->val < val) {
+		prev = next;
+		next = prev->next;
+	}
+	result = (next->val != val);
+	if (result) {
+		prev->next = new_node(val, next, 0);
+	}
+	return result;
+}	
+
+
+int set_add(intset_t *set, val_t val, int transactional)
+{
+	int result;
+
+#ifdef DEBUG
+	//	printf("++> set_add(%d)\n", (int)val);
+	//	IO_FLUSH;
+#endif
+
+	if (!transactional) {
+
+		result = set_seq_add(set, val);
+	//	printf(" 1 add seqq\n");
+	} else { 
+
+#ifdef SEQUENTIAL /* Unprotected */
+
+		result = set_seq_add(set, val);
+	//	printf(" 2 add seqq\n");
+
+#elif defined STM
+
+		node_t *prev, *next;
+		val_t v;	
+
+		if (transactional > 2) {
+//added by charlie
+			unsigned status;
+			node_t *n_node = new_node(val, NULL, transactional); 
+			if((status = _xbegin()) == _XBEGIN_STARTED) {
+				prev = set->head;
+				next = (node_t *)TX_LOAD(&prev->next);
+				while (1) {
+					v = TX_LOAD((uintptr_t *) &next->val);
+					if (v >= val)
+						break;
+					prev = next;
+					next = (node_t *)TX_LOAD(&prev->next);
+				}
+				result = (v != val);
+				if (result) {
+					n_node->next = next;
+					TX_STORE(&prev->next, n_node);
+				}
+				_xend();
+			}else{
+				//tx fail 하면 harris_insert로 
+			//	printf("add abort\n");
+				result = -1; //asking for retry
+			}
+//charie finish
+		} else {
+			printf("set_add no transactional\n");
+			TX_START(NL);
+			prev = set->head;
+			next = (node_t *)TX_LOAD(&prev->next);
+			while (1) {
+				v = TX_LOAD((uintptr_t *) &next->val);
+				if (v >= val)
+					break;
+				prev = next;
+				next = (node_t *)TX_LOAD(&prev->next);
+			}
+			result = (v != val);
+			if (result) {
+				TX_STORE(&prev->next, new_node(val, next, transactional));
+			}
+			TX_END;
+		}
+
+		//result = harris_insert(set, val);
+#endif
+
+	}
+
+	return result;
+}
+
+int set_remove(intset_t *set, val_t val, int transactional)
+{
+	int result = 0;
+
+#ifdef DEBUG
+	//	printf("++> set_remove(%d)\n", (int)val);
+	//	IO_FLUSH;
+#endif
+
+#ifdef SEQUENTIAL /* Unprotected */
+
+	node_t *prev, *next;
+
+	prev = set->head;
+	next = prev->next;
+	while (next->val < val) {
+		prev = next;
+		next = prev->next;
+	}
+	result = (next->val == val);
+	if (result) {
+		prev->next = next->next;
+		free(next);
+	}
+
+#elif defined STM
+
+	node_t *prev, *next;
+	val_t v;
+	node_t *n;
+	unsigned status;
+
+	if (transactional > 3) {
+//added by charlie
+		if((status = _xbegin()) == _XBEGIN_STARTED) {
+			prev = set->head;
+			next = (node_t *)TX_LOAD(&prev->next);
+			while (1) {
+				v = TX_LOAD((uintptr_t *) &next->val);
+				if (v >= val)
+					break;
+				prev = next;
+				next = (node_t *)TX_LOAD(&prev->next);
+			}
+			result = (v == val);
+			if (result) {
+				n = (node_t *)TX_LOAD(&next->next);
+				TX_STORE(&prev->next, n);
+			}
+			_xend();
+	//		printf("remove commited\n");
+			if(result)
+				FREE(next, sizeof(node_t));
+//charlie finish
+		}else{
+	//		printf("remove abort\n");
+			result = harris_delete(set, val);
+		}
+
+	} else {
+		TX_START(NL);
+		prev = set->head;
+		next = (node_t *)TX_LOAD(&prev->next);
+		while (1) {
+			v = TX_LOAD((uintptr_t *) &next->val);
+			if (v >= val)
+				break;
+			prev = next;
+			next = (node_t *)TX_LOAD(&prev->next);
+		}
+		result = (v == val);
+		if (result) {
+			n = (node_t *)TX_LOAD(&next->next);
+			TX_STORE(&prev->next, n);
+			FREE(next, sizeof(node_t));
+		}
+		TX_END;
+	}
+
+#elif defined LOCKFREE
+	//	result = harris_delete(set, val);
+#endif
+
+	return result;
+}
+
+
